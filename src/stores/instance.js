ef3b00c4af0c7665cc8e4c72facb5c431c392250
Sarah <S00038939@auk.edu.kw>
2020-08-16 15:30:43 +0300

    changed base url

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
-  //baseURL: "https://protected-falls-81498.herokuapp.com/",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://protected-falls-81498.herokuapp.com/",
 });
 
 export default instance;
